d03f0bd9a1bf1c54ccf2b50d8b1045d7f46bf1e7
Valentine Dmitrieva <dimasquared@gmail.com>
2022-08-01 13:06:24 +0300

    feat: implement test 2

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -12,7 +12,7 @@ public class Tests
     }
 
     [Test, Order(1)]
-    public void Test1()
+    public void SendGetRequestToGetAllPosts()
     {
         RestClient client = new RestClient("https://jsonplaceholder.typicode.com");
         RestRequest request = new RestRequest("/posts");
@@ -27,6 +27,21 @@ public class Tests
             var nextPost = posts[i+1];
             Assert.IsTrue((nextPost.id > currentPost.id), "Posts are not sorted ascending by id");
         }
+    }
+
+    [Test, Order(2)]
+    public void SendGetRequestToGetPostWithId()
+    {
+        RestClient client = new RestClient("https://jsonplaceholder.typicode.com");
+        RestRequest request = new RestRequest("/posts/99");
+        RestResponse response = client.Get(request);
+        Assert.AreEqual(200, (int)response.StatusCode, "Request returned a non-200 response");
+        
+        var post = response.Deserialize<PostData>();
+        Assert.AreEqual(10, post.userId, "userId is not correct");
+        Assert.AreEqual(99, post.id, "id is not correct");
+        Assert.IsNotEmpty(post.title, "title is empty");
+        Assert.IsNotEmpty(post.body, "body is empty");
     }
 }
 
